22bab98b5e4dc1bc140a6dc98a6bc33aabf7ef3b
Harpreet Singh <harpreet.singh@nagra.com>
2026-01-16 23:00:36 +0000

    added autoFocus logic

--- test/handleKeyDown.js
+++ test/handleKeyDown.js
@@ -1,6 +1,24 @@
-import { getNextFocus } from '../../lib/lrud.js';
+import { getNextFocus, setConfig } from '../../lib/lrud.js';
 
 let scope = null;
+
+/**
+ * setup element id's when requeted by lrud
+ */
+const ID_LIMIT = 100000; // big enough to wrap around!
+let id = 0;
+function setupId(node) {
+  let newId = null;
+  if (node) {
+    // Use a simple incremented number as id
+    newId = `lrud-${id > ID_LIMIT ? 0 : ++id}`;
+    node.id = newId;
+
+    return newId;
+  }
+
+  return newId;
+}
 
 /**
  * Perform directional navigation
@@ -24,6 +42,11 @@ window.addEventListener('click', (e) => {
   if (e.target.nodeName !== 'INPUT' && e.target.nodeName !== 'LABEL') {
     e.preventDefault();
   }
+});
+
+// Configure LRUD
+setConfig({
+  createMissingId: setupId
 });
 
 window.addEventListener('keydown', handleKeyDown);
